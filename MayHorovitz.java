106754206d8dfb6d9ef25010cbe4fffa221fccb0
mayhor <mayhorovitz@gmail.com>
2025-04-08 16:59:32 +0300

    Add Hello and May files

--- MayHorovitz.java
+++ MayHorovitz.java
@@ -1,4 +1,4 @@
-public class yourname {
+public class MayHorovitz {
     public static void main(String[] args) {
         for (int i=1;i<=10; i++) {
             System.out.println("May Horovitz");
